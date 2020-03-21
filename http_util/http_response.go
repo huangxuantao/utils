e6043b17c4690d14838db2dc94c4014762a0b142
huangxuantao <huangxuantao@foxmail.com>
2020-03-21 23:09:46 +0800

    update

--- http_util/http_response.go
+++ http_util/http_response.go
@@ -47,6 +47,31 @@ func NormalResponseJson(c *gin.Context, err *errno_util.Errno, data interface{})
 	c.JSON(http.StatusOK, r)
 }
 
+func NormalResponseJsonWithHeader(c *gin.Context, err *errno_util.Errno, data interface{}) {
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Cache-Control", "no-cache, private")
+	c.Writer.Header().Set("Connection", "keep-alive")
+
+	var r ResponseJson
+	switch obj := data.(type) {
+	case error:
+		r = ResponseJson{
+			Code:    err.Code,
+			Message: err.Message,
+			Data:    obj.Error(),
+		}
+	default:
+		r = ResponseJson{
+			Code:    err.Code,
+			Message: err.Message,
+			Data:    data,
+		}
+	}
+	c.Set("custom_code", err.Code)
+	c.JSON(http.StatusOK, r)
+}
+
 func NormalResponseHtml(c *gin.Context, template string, err *errno_util.Errno, data gin.H) {
 	c.Set("custom_code", err.Code)
 	c.HTML(http.StatusOK, template, data)
